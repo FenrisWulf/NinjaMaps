49acb2b5708523b223f925c9a249ad52cf07ce48
FenrisWulf <fenriswoolf@gmail.com>
2015-04-05 22:11:34 -0500

    abvc

--- ninja.js
+++ ninja.js
@@ -1,7 +1,24 @@
-console.log(document);
-console.log("ok");
-console.log(document.body.innerHTML);
-// var st;
-document.body.addEventListener("DOMSubtreeModified", function(){
-	document.title="NinjaMaps"
-}, false);
+var observer = new MutationObserver(function(mutations, observer) {
+  LocalMain();
+});
+
+observer.observe(document, {
+  subtree: true,
+  attributes: true,
+  childList: true,
+  characterData: true,
+  attributeOldValue: true,
+  characterDataOldValue: true
+});
+
+function LocalMain() {
+	console.log("help");
+
+  var selectors = ['div', 'span'];
+  for (var i in selectors) {
+    var selector = selectors[i];
+    $(selector + ':contains("Flights")')
+        .text("Ninja");
+    console.log("thisran");
+  }
+}
