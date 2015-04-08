7a6b17ca674ecc49c559c6a1a53595899b351449
FenrisWulf <fenriswoolf@gmail.com>
2015-04-08 15:58:50 -0500

    asdf

--- ninja.js
+++ ninja.js
@@ -11,37 +11,36 @@ observer.observe(document, {
   characterDataOldValue: true
 });
 var hasAdded = 0;
+var counter = 0;
+var scriptStr = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=drawing';
+var scriptNode = document.createElement('script');
+scriptNode.setAttribute('type', "text/javascript");
+scriptNode.setAttribute('src', scriptStr);
+console.log(scriptNode);
 function LocalMain() {
-	console.log("help");
 
+
+	console.log("help");
+// var map = document.getElementsByTagName("canvas")[0];  var drawingManager = new google.maps.drawing.DrawingManager({
+//     drawingMode: google.maps.drawing.OverlayType.MARKER,
+//     drawingControl: true,
+//     drawingControlOptions: {
+//     },
+//   });alert("test");
+//   drawingManager.setMap(map);alert(lemon);console.log(lemon);'
+
+  var st=  'var google_tile = "https://maps.google.com/maps/api/staticmap?sensor=false&center=-34.397,150.644&zoom=8&size=300x400"; var canvas = document.getElementsByTagName("canvas")[0];var context = canvas.getContext("2d");   var imageObj = new Image();imageObj.src = google_tile;imageObj.onload = function(){context.drawImage(imageObj, 0, 0);}'
   var selectors = ['div', 'span'];
-  for (var i in selectors) {
-    var selector = selectors[i];
-    // $(selector + ':contains("Flights")')
-    //     .text("Ninja");
-    // console.log("thisran");
-  }
 
   var node = document.createElement('li');
-// <li travel_mode="2" jsaction="toggleTravelMode;focus:focusTravelMode;blur:blurTravelMode" jsinstance="*3" class="travel-mode selected" jsvs="false;" jsan="7.travel-mode,7.selected,0.travel_mode,0.ved,0.jsaction" jstcache="105" ved="0CAUQ6CYoAjAA">
-//                                                 <button tabindex="2" jsan="0.tabindex" jstcache="106">
-//                                                    <div class="directions-travel-mode-button" jstcache="0">
-//                                                       <div aria-label="Walking" tooltip="Walking" class="directions-travel-mode-icon directions-walk-icon" jsan="t-r5wgmuI8U-U,7.directions-travel-mode-icon,7.directions-walk-icon,0.aria-label,0.tooltip" jstcache="107"></div>
-//                                                       <div jstcache="108" style="display: none;"></div>
-//                                                    </div>
-//                                                    <div class="directions-travel-mode-breadcrumb" style="display: none;" jstcache="109"></div>
-//                                                    <div class="directions-travel-mode-underline" jstcache="0"></div>
-//                                                 </button>
-//                                              </li>
 
-  node.setAttribute('travel_mode', "2");
   node.setAttribute('jsaction', "toggleTravelMode;focus:focusTravelMode;blur:blurTravelMode");
   node.className = "travel-mode";
-  node.setAttribute('jsinstance', "*3");
+  // node.setAttribute('jsinstance', "8");
   node.setAttribute('jsvs', "false;");
   node.setAttribute('jsan', "7.travel-mode,0.travel_mode,0.ved,0.jsaction");
-  node.setAttribute('jstcache', "105");
-  node.setAttribute('ved', "0CAUQ6CYoAjAA");
+  // node.setAttribute('jstcache', "105");
+  // node.setAttribute('ved', "4968");
 
   var d4Node = document.createElement('div');
   var d5Node = document.createElement('div');
@@ -60,7 +59,7 @@ function LocalMain() {
   d2Node.setAttribute("tooltip", "Ninja");
   d2Node.className = 'directions-travel-mode-icon directions-ninja-icon';
   // d2Node.setAttribute("jsan", "t-CxKxTMoc_RM,7.directions-travel-mode-icon,7.directions-fly-icon,0.aria-label,0.tooltip");
-  d2Node.setAttribute("jstcache", "107");
+  // d2Node.setAttribute("jstcache", "107");
   
   d3Node.setAttribute('style', 'display: none;');
   d3Node.setAttribute('jstcache', '108');
@@ -68,7 +67,9 @@ function LocalMain() {
   var butNode = document.createElement('button');
   butNode.setAttribute("tabindex", "2");
   butNode.setAttribute("jsan", "0.tabindex");
-  butNode.setAttribute("jstcache", "0");
+  butNode.setAttribute("jstcache", "106");
+
+  butNode.setAttribute("onclick", st);
 
   d1Node.appendChild(d2Node);
   d1Node.appendChild(d3Node);
@@ -77,17 +78,30 @@ function LocalMain() {
   butNode.appendChild(d4Node);
   butNode.appendChild(d5Node);
   node.appendChild(butNode);
+  
   var uls = document.getElementsByTagName('ul');
   var i = 0;
   for (i = 0; i < uls.length; i++) {
-  	var cid = uls[i].getAttribute('jstcache');
-  	// console.log(cid);
-  	if (hasAdded == 0) {
+  	var cid = uls[i].childNodes;
 
-	  	if (cid == '101') {
+  	// console.log(cid.length);
+  	if (cid.length == 4 && counter++ > 2) {
+      if (hasAdded == 0) {
+
+        hasAdded = 1;
+        // document.head.appendChild(ascriptNode);
+        document.head.appendChild(scriptNode);
+
+	  	  node.setAttribute('travel_mode', "18");
 	  		console.log('adding new element!');
-	  		uls[i].insertBefore(node, uls[i].childNodes[4]);
-	  		hasAdded = 1;
+        console.log(node);
+        console.log(butNode);
+        console.log(d1Node);
+        console.log(d2Node);
+        console.log("length "+ uls[i].length);
+	  		uls[i].insertBefore(node,  uls[i].childNodes[2]);
+        console.log('added node');
+        console.log(node);
 	  	}
   	}
   	// $('ul' + ':contains("103")').appendChild(node);
